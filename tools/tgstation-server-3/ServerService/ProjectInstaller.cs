da53a96cf0bc6f0834aeecc02e5a6e3c22aff626
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-25 09:06:23 -0400

    Start the service after install

--- tools/tgstation-server-3/ServerService/ProjectInstaller.cs
+++ tools/tgstation-server-3/ServerService/ProjectInstaller.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ServiceProcess;
 
 namespace ServerService
 {
@@ -14,6 +10,14 @@ namespace ServerService
 		public ProjectInstaller()
 		{
 			InitializeComponent();
+		}
+
+		private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+		{
+			using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+			{
+				sc.Start();
+			}
 		}
 	}
 }
