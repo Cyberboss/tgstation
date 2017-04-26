bfdf6060072a2efe75c3a5b9891d0f15ee75cf3e
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-26 18:53:23 -0400

    Fucking defaults

--- tools/tgstation-server-3/ServerService/Properties/Settings1.Designer.cs
+++ tools/tgstation-server-3/ServerService/Properties/Settings1.Designer.cs
@@ -193,13 +193,9 @@ namespace TGServerService.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
-<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-  <string>#tgstation13</string>
-  <string>#devbus</string>
-  <string>#coderbus</string>
-  <string>#adminbus</string>
-</ArrayOfString>")]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
+            "tring>#botbus</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection IRCChannels {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["IRCChannels"]));
@@ -235,7 +231,7 @@ namespace TGServerService.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("#adminbus")]
+        [global::System.Configuration.DefaultSettingValueAttribute("#botbus")]
         public string IRCAdminChannel {
             get {
                 return ((string)(this["IRCAdminChannel"]));
