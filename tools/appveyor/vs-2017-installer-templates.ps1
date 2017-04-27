8ae19dca5046469bc4782f4d46dae46f9af4cd0a
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-27 19:15:47 -0400

    REEE

--- tools/appveyor/vs-2017-installer-templates.ps1
+++ tools/appveyor/vs-2017-installer-templates.ps1
@@ -1,3 +1,3 @@
 $vsixPath = "$($env:USERPROFILE)\InstallerProjects.vsix"
 (New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/fd136a01-a0c8-475f-94dd-240136f86746/file/247375/3/InstallerProjects.vsix', $vsixPath)
-& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe" /q /a $vsixPath
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe" /q /a /skuName:Community /skuVersion:15.0 $vsixPath
