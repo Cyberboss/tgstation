579faced4fac5757cc709a3c39c28b242955d026
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-27 17:05:46 -0400

    FixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixitFixit

--- tools/appveyor/build-tgs3-installer.cmd
+++ tools/appveyor/build-tgs3-installer.cmd
@@ -1,2 +1,2 @@
 set DEVENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"
-%DEVENV% tools/tgstation-server-3/TGStationServer3.sln /Build Release
+%DEVENV% tools/tgstation-server-3/TGStationServer3.sln /Build Release /Project TGServiceInstaller /Out errorFile.txt
