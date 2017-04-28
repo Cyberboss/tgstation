50888bfa6a55af57705dabd6cb8190d9ae7385ec
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-27 23:03:37 -0400

    alsdjkf;lkajsd;lf

--- tools/appveyor/ZipTGS3Release.ps1
+++ tools/appveyor/ZipTGS3Release.ps1
@@ -1,7 +1,7 @@
-$destination = "TGS3.zip"
+$destination = "$(Env:APPVEYOR_BUILD_FOLDER)TGS3.zip"
 
  If(Test-path $destination) {Remove-item $destination}
 
 Add-Type -assembly "system.io.compression.filesystem"
 
-[io.compression.zipfile]::CreateFromDirectory("($Env:APPVEYOR_BUILD_FOLDER)tools/tgstation-server-3/TGServiceInstaller/bin/Release", $destination) 
+[io.compression.zipfile]::CreateFromDirectory("$(Env:APPVEYOR_BUILD_FOLDER)tools\tgstation-server-3\TGServiceInstaller\bin\Release", $destination) 
