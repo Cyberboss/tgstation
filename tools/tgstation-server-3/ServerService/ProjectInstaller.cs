6e989c0619134881194e11c59ebbcca2a001e15f
Cyberboss <Cyberboss@users.noreply.github.com>
2017-04-27 23:32:05 -0400

    Cleanup

--- tools/tgstation-server-3/ServerService/ProjectInstaller.cs
+++ tools/tgstation-server-3/ServerService/ProjectInstaller.cs
@@ -5,13 +5,18 @@ using System.ServiceProcess;
 namespace ServerService
 {
 	[RunInstaller(true)]
-	public partial class ProjectInstaller : System.Configuration.Install.Installer
+	public partial class ProjectInstaller : Installer
 	{
 		public ProjectInstaller()
 		{
 			InitializeComponent();
+			serviceInstaller1.BeforeUninstall += ServiceInstaller1_BeforeUninstall;
 		}
-		
+
+		private void ServiceInstaller1_BeforeUninstall(object sender, InstallEventArgs e)
+		{
+		}
+
 		private void ServiceInstaller1_AfterInstall(object sender, InstallEventArgs e)
 		{
 			using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
