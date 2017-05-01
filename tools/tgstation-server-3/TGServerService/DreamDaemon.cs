89632895fc19c29b40b68044a3de91838c7038c2
Cyberboss <Cyberboss@users.noreply.github.com>
2017-05-01 15:13:17 -0400

    Remove uneedful references

--- tools/tgstation-server-3/TGServerService/DreamDaemon.cs
+++ tools/tgstation-server-3/TGServerService/DreamDaemon.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Windows.Forms;
 using TGServiceInterface;
 
 namespace TGServerService
