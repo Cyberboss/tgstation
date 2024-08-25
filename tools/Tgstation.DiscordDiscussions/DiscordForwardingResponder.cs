23527d6f9a32a046557692e2df6e16b45a957f64
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-08-25 11:40:07 -0400

    asdfsadf

---
+++ tools/Tgstation.DiscordDiscussions/DiscordForwardingResponder.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Tgstation.DiscordDiscussions
+{
+	/// <summary>
+	/// An <see cref="IResponder{TGatewayEvent}"/> that forwards to another <see cref="targetResponder"/>.
+	/// </summary>
+	sealed class DiscordForwardingResponder : IResponder<IReady>
+	{
+		/// <summary>
+		/// The <see cref="IResponder{TGatewayEvent}"/> to forward the event to.
+		/// </summary>
+		readonly IResponder<IReady> targetResponder;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DiscordForwardingResponder"/> class.
+		/// </summary>
+		/// <param name="targetResponder">The value of <see cref="targetResponder"/>.</param>
+		public DiscordForwardingResponder(IResponder<IReady> targetResponder)
+		{
+			this.targetResponder = targetResponder ?? throw new ArgumentNullException(nameof(targetResponder));
+		}
+
+		/// <inheritdoc />
+		public Task<Result> RespondAsync(IReady gatewayEvent, CancellationToken ct) => targetResponder.RespondAsync(gatewayEvent, ct);
+	}
+}
